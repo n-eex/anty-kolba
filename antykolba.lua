41d88a285b94624bb42d5a99daba5f2254f1f311
n-eex <neex.kontakt@gmail.com>
2021-07-26 14:26:32 +0200

    Add new files

---
+++ antykolba.lua
@@ -0,0 +1,18 @@
+---- Created by neex#9862
+---- Support discord https://discord.gg/dT85kYawe2
+
+Citizen.CreateThread(function())
+    while true do
+        Citizen.Wait(0)
+
+        local ped = GetPlayerPed(-1)
+        IsPlayerAdmed(god, 6) then
+            DisableControlAction(1, 140, true)
+            DisableControlAction(1, 141, true)
+            DisableControlAction(1, 142, true)
+        end
+    end
+end)
+
+---- Created by neex#9862
+---- Support discord https://discord.gg/dT85kYawe2
